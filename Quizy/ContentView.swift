c6d26b16b31251a20d97cda064f3b277fee6a719
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-09 15:36:42 +0530

    Initial Commit

---
+++ Quizy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Quizy
+//
+//  Created by Bhumika Patel on 09/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quizy/QuizyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizyApp.swift
+//  Quizy
+//
+//  Created by Bhumika Patel on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
