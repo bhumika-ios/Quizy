30bb33a806636e6781beef73c4372a9be14e1ad6
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-09 17:17:09 +0530

    create string for first latter convert in capital

---
+++ Quizy/Extension/String.swift
@@ -0,0 +1,27 @@
+//
+//  String.swift
+//  Quizy
+//
+//  Created by Bhumika Patel on 09/06/23.
+//
+
+import Foundation
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
+
+extension StringProtocol {
+    var html2AttributedString: NSAttributedString? {
+        Data(utf8).html2AttributedString
+    }
+    var html2String: String {
+        html2AttributedString?.string ?? ""
+    }
+}
