210dc56fa55cd64e7cae6474e3265f5772ea4de2
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-10 14:33:09 +0530

    Vstack under add zstack set background color and image set then frame set of scrollview then custom corner add in clip shape

--- Quizy/View/CategoryView.swift
+++ Quizy/View/CategoryView.swift
@@ -15,37 +15,65 @@ struct CategoryView: View {
         ]
     var body: some View {
         NavigationView{
-            ZStack{
-                Color("BG")
-                    .edgesIgnoringSafeArea(.all)
+            
                 VStack{
-                    ScrollView(showsIndicators: false) {
-                        LazyVGrid(columns: columns, spacing: 10) {
-                            ForEach(0 ..< Manager.API.QuestionCategory.allCases.count, id: \.self) {cat in
-                                Button(action: {
-                                    //   isPressed = true
-                                }, label: {
-                                    Text(Manager.API.QuestionCategory.allCases[cat].categoryName)
-                                        .foregroundColor(cat == selectedCategoryIndex ? .white : .purple)
+                    ZStack{
+                        Color("Blue")
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    Image("q1")
+                            .resizable()
+                            .frame(width: 150,height: 80)
+                            .offset(x:-105,y:-165)
+                        
+                        Image("q2")
+                            .resizable()
+                            .frame(width: 450,height: 220)
+                            .offset(y:-20)
+                }
+                    
+                    ZStack{
+                        Color("Blue")
+                        .edgesIgnoringSafeArea(.all)
+                        VStack{
+                            ScrollView(showsIndicators: false) {
+                                LazyVGrid(columns: columns, spacing: 10) {
+                                    ForEach(0 ..< Manager.API.QuestionCategory.allCases.count, id: \.self) {cat in
+                                        Button(action: {
+                                            //   isPressed = true
+                                        }, label: {
+                                            Text(Manager.API.QuestionCategory.allCases[cat].categoryName)
+                                                .foregroundColor(cat == selectedCategoryIndex ? .white : .purple)
+                                                .onTapGesture {
+                                                    selectedCategoryIndex = cat
+                                                }
+                                        })
+                                        .padding()
+                                        .frame(width: 140, height: 80)
+                                        .background(cat == selectedCategoryIndex  ? Color.purple : Color.white)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
                                         .onTapGesture {
                                             selectedCategoryIndex = cat
                                         }
-                                })
-                                .padding()
-                                .frame(width: 140, height: 80)
-                                .background(cat == selectedCategoryIndex  ? Color.purple : Color.white)
-                                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                                .onTapGesture {
-                                    selectedCategoryIndex = cat
+                                        .padding(10)
+                                    }
+                                    // .padding()
                                 }
-                                .padding()
                             }
-                            // .padding()
+                            .frame(maxWidth:.infinity , maxHeight: .infinity)
+                            .background(
+                                Color("BG")
+                                // apply custom corner
+                                    .clipShape(CustomCorner(corner: [.topLeft,.topRight], radius: 25))
+                                    .ignoresSafeArea()
+                            )
                         }
+                        // .padding()
                     }
-                }
-               // .padding()
+                    .padding(.vertical,-85)
+              //  }
             }
+            
         }
     }
 }
