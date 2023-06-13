ecb0875dad223c7a81d77dd95093d99551eea3ff
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-13 16:17:47 +0530

    create conclusion for your show progress repot -  graphically represent data and number wise show repot and again start button calling

--- Quizy/View/ConclusionView.swift
+++ Quizy/View/ConclusionView.swift
@@ -14,97 +14,114 @@ extension Views {
         @State var loadingProgress: Int = 0
 
         var body: some View {
-            ZStack{
-                Color("BlueLight1")
-                    .edgesIgnoringSafeArea(.all)
-                VStack {
-                    ZStack {
-                        Circle()
-                            .trim(from: Views.ConclusionView.CircleConstant.trimBegin,
-                                  to: Views.ConclusionView.CircleConstant.trimTarget)
-                            .stroke(Color.red,
-                                    style: StrokeStyle(lineWidth: Views.ConclusionView.CircleConstant.defaultLineWidth))
-                            .frame(width: Views.ConclusionView.CircleConstant.frameSize,
-                                   height: Views.ConclusionView.CircleConstant.frameSize)
-                            .rotationEffect((Angle(degrees: Views.ConclusionView.CircleConstant.defaultAngle)))
-                        Circle()
-                            .trim(from: Views.ConclusionView.CircleConstant.trimBegin,
-                                  to: Views.ConclusionView.CircleConstant.animatedTrimTarget(target: loadingProgress))
-                            .stroke(Color.blue, lineWidth: Views.ConclusionView.CircleConstant.defaultLineWidth)
-                            .frame(width: Views.ConclusionView.CircleConstant.frameSize,
-                                   height: Views.ConclusionView.CircleConstant.frameSize)
-                            .rotationEffect(Angle(degrees: Views.ConclusionView.CircleConstant.defaultAngle))
-                        Text("\(viewModel.answerRate())%")
-                            .font(.system(size: 32))
-                            .fontWeight(.semibold)
-                    }
-                    .offset(y:30)
-                    .background{
-                        RoundedRectangle(cornerRadius: 8)
-                            .fill(.white)
-                            .frame(width: 350, height: 180)
-                    }
-                    .offset(y: -150)
-                    .padding(10)
-                    Text("Finished Quiz")
-                        .foregroundColor(Color("Blue"))
-                        .font(.system(size: 22))
-                        .fontWeight(.bold)
-                        .offset(y: -150)
-                    VStack(spacing: DesignSystem.Padding.microPadding) {
-                        VStack{
-                            // List{
-                            //   Section{
-                            VStack{
-                                HStack{
-                                    Text("Correct Answers:")
-                                    Spacer()
-                                    Text("\(viewModel.correctAnswers)")
-                                }
-                            }
-                            Divider()
-                            VStack{
-                                HStack{
-                                    Text("Incorrect Answers:")
-                                    Spacer()
-                                    Text("\(viewModel.wrongAnswers)")
-                                }
-                            }
-                            Divider()
-                            HStack{
-                                Text("Total Questions Answers:")
-                                Spacer()
-                                Text("\(viewModel.totalQuestions)")
-                            }
+            NavigationView{
+                ZStack{
+                    Color("BlueLight1")
+                        .edgesIgnoringSafeArea(.all)
+                    VStack {
+                        ZStack {
+                            Circle()
+                                .trim(from: Views.ConclusionView.CircleConstant.trimBegin,
+                                      to: Views.ConclusionView.CircleConstant.trimTarget)
+                                .stroke(Color.red,
+                                        style: StrokeStyle(lineWidth: Views.ConclusionView.CircleConstant.defaultLineWidth))
+                                .frame(width: Views.ConclusionView.CircleConstant.frameSize,
+                                       height: Views.ConclusionView.CircleConstant.frameSize)
+                                .rotationEffect((Angle(degrees: Views.ConclusionView.CircleConstant.defaultAngle)))
+                            Circle()
+                                .trim(from: Views.ConclusionView.CircleConstant.trimBegin,
+                                      to: Views.ConclusionView.CircleConstant.animatedTrimTarget(target: loadingProgress))
+                                .stroke(Color.blue, lineWidth: Views.ConclusionView.CircleConstant.defaultLineWidth)
+                                .frame(width: Views.ConclusionView.CircleConstant.frameSize,
+                                       height: Views.ConclusionView.CircleConstant.frameSize)
+                                .rotationEffect(Angle(degrees: Views.ConclusionView.CircleConstant.defaultAngle))
+                            Text("\(viewModel.answerRate())%")
+                                .font(.system(size: 32))
+                                .fontWeight(.semibold)
                         }
-                        .padding(.horizontal,15)
-//                        .background{
-//                            RoundedRectangle(cornerRadius: 8)
-//                                .fill(Color("Blue"))
-//                                .frame(width: 300, height: 200)
-//                        }
-                           // }
-                          
-                        .padding(.vertical,-95)
-                       // }
-                        VStack{
-                            NavigationLink(destination: CategoryView(viewModel: .init()).navigationBarHidden(true)) {
-                                Text("Start Again")
-                                    .foregroundColor(.white)
-                                    .fontWeight(.bold)
-                            }
-                        }
+                        .offset(y:30)
                         .background{
                             RoundedRectangle(cornerRadius: 8)
-                                .fill(Color("Blue"))
-                                .frame(width: 400, height: 52)
+                                .fill(.white)
+                                .frame(width: 350, height: 180)
                         }
-                        .offset(y: 275)
+                        .offset(y: -150)
+                        .padding(10)
+                        Text("Your Score")
+                            .foregroundColor(Color("Blue"))
+                            .font(.system(size: 22))
+                            .fontWeight(.bold)
+                            .offset(y: -150)
+                        VStack(spacing: DesignSystem.Padding.microPadding) {
+                            VStack{
+                                // List{
+                                //   Section{
+                                VStack{
+                                    HStack{
+                                        Text("Correct Answers:")
+                                        Spacer()
+                                        Text("\(viewModel.correctAnswers)")
+                                            .fontWeight(.bold)
+                                    }
+                                    .foregroundColor(.green)
+                                }
+                                Divider()
+                                VStack{
+                                    HStack{
+                                        Text("Incorrect Answers:")
+                                        Spacer()
+                                        Text("\(viewModel.wrongAnswers)")
+                                            .fontWeight(.bold)
+                                    }
+                                    .foregroundColor(.red)
+                                }
+                              
+                                Divider()
+                                HStack{
+                                    Text("Total Questions Answers:")
+                                    Spacer()
+                                    Text("\(viewModel.totalQuestions)")
+                                        .fontWeight(.bold)
+                                }
+                                .foregroundColor(Color("Blue"))
+                            }
+                            .padding(.horizontal,25)
+                            .background{
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(Color("BlueLight2"))
+                                    .frame(width: 350, height: 140)
+                                    .padding(.vertical,-15)
+                            }
+                            .padding(10)
+                          
+                            // }
+                            
+                            .padding(.vertical,-95)
+                            // }
+                            VStack{
+                                NavigationLink(destination: CategoryView(viewModel: .init()).navigationBarHidden(true)) {
+                                    Text("Start Again")
+                                        .font(.system(size: 22))
+                                        .foregroundColor(.white)
+                                        .fontWeight(.bold)
+                                        .offset(y: -2)
+                                        .background{
+                                            RoundedRectangle(cornerRadius: 8)
+                                                .fill(Color("Blue"))
+                                                .frame(width: 400, height: 55)
+                                        }
+                                }
+                            }
+                            
+                            .offset(y: 275)
+                        }
+                    }.task {
+                        animateCircle()
                     }
-                }.task {
-                    animateCircle()
                 }
+              
             }
+            
         }
         func animateCircle() {
             _ = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
