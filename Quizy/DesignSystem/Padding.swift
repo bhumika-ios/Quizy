9290d1d50e6979a87e13878d46c97f822289c7bc
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-13 16:12:53 +0530

    create padding class but not use mostly

---
+++ Quizy/DesignSystem/Padding.swift
@@ -0,0 +1,16 @@
+//
+//  Padding.swift
+//  Quizy
+//
+//  Created by Bhumika Patel on 10/06/23.
+//
+
+import SwiftUI
+
+extension DesignSystem {
+    struct Padding {
+        static let microPadding: CGFloat = 10
+        static let padding: CGFloat = 10
+        static let macroPadding: CGFloat = 10
+    }
+}
