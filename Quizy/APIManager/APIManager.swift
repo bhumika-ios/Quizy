96a1ede4d322396a8178c99781f6bd8aa3e60ecc
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-13 16:09:14 +0530

    change some code of manger api

--- Quizy/APIManager/APIManager.swift
+++ Quizy/APIManager/APIManager.swift
@@ -21,21 +21,78 @@ extension Manager {
             case invalidCategory
         }
 
+//        enum Difficulty: String, CaseIterable {
+//            case any
+//            case easy
+//            case medium
+//            case hard
+//        }
+
+        enum AnswerTypes: CaseIterable {
+            case any
+            case multiple
+            case boolean
+
+            static func withLabel(_ label: String) -> AnswerTypes {
+                let caseResult = self.allCases.first {
+                    $0.rawValue == label
+                }
+                guard let caseResult = caseResult else {
+                    return .multiple
+                }
+                return caseResult
+            }
+
+            var answerTypeName: String {
+                switch self {
+                case .any:
+                    return "Any question type"
+                case .multiple:
+                    return "Multiple Choices"
+                case .boolean:
+                    return "Right or Wrong"
+                }
+            }
+
+            var rawValue: String {
+                switch self {
+                case .any:
+                    return "any"
+                case .multiple:
+                    return "multiple"
+                case .boolean:
+                    return "boolean"
+                }
+            }
+        }
+
         func queryBuilder(
+//            difficulty: Difficulty,
             category: QuestionCategory,
+            answerType: AnswerTypes,
             amount: Int = 10
         ) throws -> URL {
             var components = URLComponents()
             components.scheme = "https"
             components.host = "opentdb.com"
-            components.path = "/api.php?"
+            components.path = "/api.php"
 
             var queryItems: [URLQueryItem] = [
-                URLQueryItem(name: "amount=", value: String(amount))
+                URLQueryItem(name: "amount", value: String(amount))
             ]
-            if category != .all {
-                queryItems.append(URLQueryItem(name: "&category=", value: String(category.categoryId)))
+
+//            if difficulty != .any {
+//                queryItems.append(URLQueryItem(name: "difficulty", value: difficulty.rawValue as String))
+//            }
+
+            if category != .generalKnowledge {
+                queryItems.append(URLQueryItem(name: "category", value: String(category.categoryId)))
             }
+
+            if answerType != .multiple {
+                queryItems.append(URLQueryItem(name: "type", value: answerType.rawValue))
+            }
+
             components.queryItems = queryItems
             guard let url = components.url else { throw QuestionError.badURL }
             return url
@@ -43,10 +100,14 @@ extension Manager {
 
         mutating func fetchQuestions(
             category: QuestionCategory,
+//            difficulty: Difficulty,
+            answerType: AnswerTypes,
             amount: Int = 10
         ) async throws -> [Question] {
             let url = try queryBuilder(
+//                difficulty: difficulty,
                 category: category,
+                answerType: answerType,
                 amount: amount
             )
             let session = URLSession(configuration: .ephemeral)
@@ -63,6 +124,8 @@ extension Manager {
             return questions.map { question in
                 return Question(
                     category: QuestionCategory.withLabel(question.category),
+                    type: AnswerTypes.withLabel(question.type),
+                    difficulty: question.difficulty,
                     question: question.question.html2String,
                     correct_answer: question.correct_answer.html2String,
                     incorrect_answers: question.incorrect_answers.map { answer in
@@ -77,7 +140,8 @@ extension Manager {
 // swiftlint:disable:all identifier_name
 public struct Question {
     let category: Manager.API.QuestionCategory
-   
+    let type: Manager.API.AnswerTypes
+    let difficulty: String
     let question: String
     let correct_answer: String
     let incorrect_answers: [String]
@@ -92,7 +156,8 @@ extension Manager.API {
 
     struct QuestionAPI: Codable {
         let category: String
-        
+        let type: String
+        let difficulty: String
         let question: String
         let correct_answer: String
         let incorrect_answers: [String]
@@ -101,7 +166,7 @@ extension Manager.API {
 
 extension Manager.API {
     public enum QuestionCategory: CaseIterable {
-        case all
+ //       case all
         case generalKnowledge
 //        case entertainmentBooks
 //        case entertainmentFilms
@@ -119,9 +184,9 @@ extension Manager.API {
         case history
         case politics
         case art
-        case celebrities
+//        case celebrities
         case animals
-        case vehicles
+//        case vehicles
 //        case entertainmentComics
 //        case scienceGadgets
 //        case entertainmentAnimeAndMaga
@@ -132,23 +197,23 @@ extension Manager.API {
                 $0.categoryName == label
             }
             guard let caseResult = caseResult else {
-                return .all
+                return .generalKnowledge
             }
             return caseResult
         }
 
         var categoryName: String {
             switch self {
-            case .all:
-                return "All"
+//            case .all:
+//                return "All"
             case .generalKnowledge:
                 return "General Knowledge"
 //            case .entertainmentBooks:
-//                return "Books"
+//                return "Entertainment: Books"
 //            case .entertainmentFilms:
-//                return "Film"
+//                return "Entertainment: Film"
 //            case .entertainmentMusic:
-//                return "Music"
+//                return "Entertainment: Music"
 //            case .entertainmentMusicalsAndTheatres:
 //                return "Entertainment: Musicals & Theatres"
 //            case .entertainmentTelevision:
@@ -158,11 +223,11 @@ extension Manager.API {
 //            case .entertainmentBoardGames:
 //                return "Entertainment: Board Games"
             case .scienceAndNature:
-                return "Science"
+                return "Science & Nature"
             case .scienceComputers:
-                return "Computers"
+                return "Science: Computers"
             case .scienceMathematics:
-                return "Mathematics"
+                return "Science: Mathematics"
             case .mythology:
                 return "Mythology"
             case .sports:
@@ -175,12 +240,12 @@ extension Manager.API {
                 return "Politics"
             case .art:
                 return "Art"
-            case .celebrities:
-                return "Celebrities"
+//            case .celebrities:
+//                return "Celebrities"
             case .animals:
                 return "Animals"
-            case .vehicles:
-                return "Vehicles"
+//            case .vehicles:
+//                return "Vehicles"
 //            case .entertainmentComics:
 //                return "Entertainment: Comics"
 //            case .scienceGadgets:
@@ -189,14 +254,14 @@ extension Manager.API {
 //                return "Entertainment: Japanese Anime & Manga"
 //            case .entertainmentCartoonAndAnimations:
 //                return "Entertainment: Cartoon & Animations"
-          
+            
             }
         }
 
         var categoryId: Int {
             switch self {
-            case .all:
-                return 0
+//            case .all:
+//                return 0
             case .generalKnowledge:
                 return 9
 //            case .entertainmentBooks:
@@ -231,12 +296,12 @@ extension Manager.API {
                 return 24
             case .art:
                 return 25
-            case .celebrities:
-                return 26
+//            case .celebrities:
+//                return 26
             case .animals:
                 return 27
-            case .vehicles:
-                return 28
+//            case .vehicles:
+//                return 28
 //            case .entertainmentComics:
 //                return 29
 //            case .scienceGadgets:
@@ -249,4 +314,3 @@ extension Manager.API {
         }
     }
 }
-
