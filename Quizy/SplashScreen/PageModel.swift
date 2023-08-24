d33151c3e2fd3194fc03340ab3cf6c6499eb0a02
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-08-24 18:00:01 +0530

    some changes

--- Quizy/SplashScreen/PageModel.swift
+++ Quizy/SplashScreen/PageModel.swift
@@ -17,7 +17,7 @@ struct PageModel: Identifiable, Hashable{
 }
 
 var pageModel: [PageModel] = [
-    .init(image: "P3", title: "Quiz with the Best", subTitle: ""),
+    .init(image: "quiz logo", title: "Quiz with the Best", subTitle: ""),
     .init(image: "P1", title: "Explore knowledge", subTitle: "Explore various categories and expand your knowledge"),
     .init(image: "P2", title: "Climb leaderboard", subTitle: "Compete for the top score and climb the leaderboard", displayAction: true),
     
